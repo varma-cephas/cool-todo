5b9a49b645615d3387144f70f77808319b98f7c1
Varma Cephas <varmac231@gmail.com>
2025-03-16 15:56:34 +0200

    feature: Add new route for todo list and todo list component. stored todolist items in an array

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,6 +8,8 @@ const loadComponents = async(id, url)=>{
     
     input_todo_form.addEventListener("submit", e=>{
         e.preventDefault();
+        const todo_item=document.createElement("li");
+
         const fd= new FormData(input_todo_form);
         fetch("/", {
             method: "POST",
@@ -16,11 +18,27 @@ const loadComponents = async(id, url)=>{
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         })
+        todo_item.textContent=input_field.value
+        document.getElementById("todo_list_item").append(todo_item)
         input_field.value="";
     })
+
+    const getTodo= async()=>{
+        const request = await fetch("/todo_arr");
+        const response = await request.json();
+        if(response.length>=1){
+            for(const item of response){
+                const todo_item=document.createElement("li");
+                todo_item.textContent=item;
+                document.getElementById("todo_list_item").append(todo_item)
+            }
+        }
+    }
+    getTodo()
 }
 
 window.onload=()=>{
+    loadComponents('todo_list', "./dist/sections/todo_list.html");
     loadComponents('todo_form', "./dist/sections/todo_form.html");
 }
 
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 
 const app = express();
-
+const arrOfTodo=[];
 //get port from env or use indicated port
 const PORT= process.env.PORT || 3000;
 
@@ -23,9 +23,14 @@ app.use(express.static(path.join(__dirname, "../")));
 //gets the form element
 app.use(express.urlencoded({extended:true}))
 
+app.get("/todo_arr", (req, res)=>{
+    res.send(arrOfTodo)
+})
+
 app.post("/", async (req, res)=>{
     const todoItem= await req.body.todo_item;
-    console.log(todoItem)
+    arrOfTodo.push(todoItem);
+    console.log(arrOfTodo)
     res.status(200)
 })
 
